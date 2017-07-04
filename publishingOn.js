afa10a7bd0ae3c154f13395b67f030a080715700
EdvardGilz <eduardo.michell94@gmail.com>
2017-07-04 12:21:17 -0500

    Carrusel

--- publishingOn.js
+++ publishingOn.js
@@ -1,13 +1,40 @@
-$.fn.publishingB = function() {
+$.fn.publishingB = function(options) {
     var id = $(this).attr('id');
+    var settings = $.extend({
+        type: "static"
+    }, options);
 
     $.getJSON("http://bixnia.com/" + id + ".json", function(data) {
-        var html = '<div style="background-color:' + data.background + '; text-align: ' + data.align + '; font-family: arial; padding: ' + data.padding + '; max-width: ' + data.width + '; min-height: ' + data.height + '; color: ' + data.color + ';">'
-        + '<img src="' + data.img + '" alt="' + data.alt + '" style="width:' + data.imgWidth + ';"/>'
-        + '<h3 style="margin: ' + data.tituloMargin + '; line-height: ' + data.tituloHeight + ';">' + data.titulo + '</h3>'
-        + '<p style="margin: ' + data.textoMargin + '; line-height: ' + data.textoHeight + ';">' + data.texto + '</p>'
-        + '</div>'
-        
-        $("#" + id).append(html);
+        if (settings.type == "static") {
+            var html = '<div style="background-color:' + data.background + '; text-align: ' + data.align + '; font-family: arial; padding: ' + data.padding + '; max-width: ' + data.width + '; min-height: ' + data.height + '; color: ' + data.color + ';">'
+                + '<img src="' + data.img + '" alt="' + data.alt + '" style="width:' + data.imgWidth + ';"/>'
+                + '<h3 style="margin: ' + data.tituloMargin + '; line-height: ' + data.tituloHeight + ';">' + data.titulo + '</h3>'
+                + '<p style="margin: ' + data.textoMargin + '; line-height: ' + data.textoHeight + ';">' + data.texto + '</p>'
+                + '</div>'
+            
+            $("#" + id).append(html);
+        }
+        else if (settings.type == "rich") {
+            var html = '<div class="swiper-container" style="background-color:' + data.background + '; text-align: ' + data.align + '; font-family: arial; padding: ' + data.padding + '; max-width: ' + data.width + '; max-height: ' + data.height + '; color: ' + data.color + ';">'
+                + '<div class="swiper-wrapper">'
+                + '<div class="swiper-slide">Real time ads</div>'
+                + '<div class="swiper-slide">Publishing On</div>'
+                + '<div class="swiper-slide">By <br> <img src="' + data.img + '" alt="' + data.alt + '" style="width:' + data.imgWidth + ';"/> </div>'
+                + '</div>'
+                + '<div class="swiper-button-prev"></div>'
+                + '<div class="swiper-button-next"></div>'
+                + '</div>'
+            
+            $("#" + id).append(html);
+
+            // http://idangero.us/swiper/api/#.WVu844q23EY
+            var mySwiper = new Swiper ('.swiper-container', {
+                loop: true,
+                autoplay: true,
+                speed: 1000,
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev'
+            })
+        }
     });
 };
