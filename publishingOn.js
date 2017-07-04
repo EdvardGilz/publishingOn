095450e77f76d82bde2b2431a8da9f67f4f7730a
EdvardGilz <eduardo.michell94@gmail.com>
2017-07-03 21:45:35 -0500

    Correccion del href para dc

--- publishingOn.js
+++ publishingOn.js
@@ -2,8 +2,6 @@ $.fn.publishingB = function() {
     var id = $(this).attr('id');
 
     $.getJSON("http://bixnia.com/" + id + ".json", function(data) {
-        $("#" + id).attr("href", data.hrefDC);
-        
         var html = '<div style="background-color:' + data.background + '; text-align: ' + data.align + '; font-family: arial; padding: ' + data.padding + '; max-width: ' + data.width + '; min-height: ' + data.height + '; color: ' + data.color + ';">'
         + '<img src="' + data.img + '" alt="' + data.alt + '" style="width:' + data.imgWidth + ';"/>'
         + '<h3 style="margin: ' + data.tituloMargin + '; line-height: ' + data.tituloHeight + ';">' + data.titulo + '</h3>'
